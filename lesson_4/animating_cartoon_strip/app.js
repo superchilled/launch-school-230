72178c0bb211c028965645f7a6994adf55520fca
superchilled <karl@superchilled.co.uk>
2018-10-05 17:11:53 +0100

    Adding start again functionality to cartoon animation.

--- lesson_4/animating_cartoon_strip/app.js
+++ lesson_4/animating_cartoon_strip/app.js
@@ -1,8 +1,20 @@
 $(function() {
   var $blinds = $("div[id^='blind']");
-  (function hidenext(blinds){
-    blinds.first().fadeOut(2000, function(){
-        (blinds=blinds.slice(1)).length && hidenext(blinds);
-    });
-  })($blinds);
+
+  function startAnimation() {
+    (function hidenext(blinds){
+      blinds.first().fadeOut(2000, function(){
+          (blinds=blinds.slice(1)).length && hidenext(blinds);
+      });
+    })($blinds);
+  };
+
+  $('a').on('click', function(event) {
+    event.preventDefault();
+    $blinds.finish().show(); // finish() is needed here to prevent two sets of
+                             // animations running simultaneously 
+    startAnimation()
+  });
+
+  startAnimation()
 });
