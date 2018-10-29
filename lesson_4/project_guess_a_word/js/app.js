c36404df94236d71d5848fc2fc54d9ac06d731be
superchilled <karl@superchilled.co.uk>
2018-10-29 16:46:13 +0000

    Adding final guess a word solution code.

--- lesson_4/project_guess_a_word/js/app.js
+++ lesson_4/project_guess_a_word/js/app.js
@@ -1,4 +1,11 @@
 $(function() {
+  var $wordContainer = $('#spaces');
+  var $guessesContainer = $('#guesses');
+  var $replayLink = $('#replay');
+  var $message = $('#message');
+  var $body = $('body');
+  var $apples = $('#apples');
+
   var randomWord = (function() {
     var words = ['apple', 'banana', 'orange', 'pear', 'strawberry'];
 
@@ -18,26 +25,94 @@ $(function() {
         this.lettersGuessed = [];
         return this;
       },
+      startGame: function() {
+        $replayLink.hide();
+        if (this.word) {
+          this.createBlanks();
+          $(document).keypress(gameEngine);
+        } else {
+          $message.text("Sorry, I've run out of words!");
+        }
+      },
+      reset: function() {
+        $message.text('');
+        $body.removeClass();
+        $apples.removeClass();
+        $wordContainer.find('span').remove();
+        $guessesContainer.find('span').remove();
+        this.init();
+        this.startGame();
+      },
       createBlanks: function() {
         var wordLength = this.word.length;
-        var $wordContainer = $('#spaces');
         $wordContainer.find('span').remove();
         var $letterSpace = $('<span></span>');
         for (var i = 0; i < wordLength; i++) {
           $wordContainer.append($letterSpace.clone());
         }
       },
+      validLetter: function(letter) {
+        return letter >= 'a' && letter <= 'z' && !this.lettersGuessed.includes(letter)
+      },
+      findIndices: function(letter) {
+        var indices = [];
+        for (var i = 0; i < this.word.length; i++) {
+          if (this.word[i] === letter) {
+            indices.push(i);
+          }
+        }
+        return indices;
+      },
+      gameWon: function() {
+        return $('#spaces span').text() === this.word;
+      },
+      guessesRemaining: function() {
+        return this.incorrectGuesses >= this.allowedGuesses;
+      },
+      endGame: function(message, className) {
+        $message.text(message);
+        $body.attr('class', className);
+        $replayLink.show();
+        $(document).unbind('keypress');
+      },
   };
 
-  for (var i = 0; i < 3; i++) {
-    var newGame = Object.create(Game).init();
+  var game = Object.create(Game).init();
+  game.startGame();
 
-    if (newGame.word) {
-      console.log(newGame.word);
-      newGame.createBlanks();
-    } else {
-      $('#message').text("Sorry, I've run out of words!");
-      $('#replay').remove();
+  function gameEngine(event) {
+    var pressedKey = event.key;
+
+    if (game.validLetter(pressedKey)) {
+      game.lettersGuessed.push(pressedKey);
+      $guessesContainer.append('<span>' + pressedKey + '</span>');
+
+      if (game.word.includes(pressedKey)) {
+        var letterIndices = game.findIndices(pressedKey);
+        var $spans = $('#spaces span');
+        letterIndices.forEach(function(index) {
+          $($spans[index]).text(pressedKey);
+        });
+      } else {
+        game.incorrectGuesses += 1;
+        var newClass = 'guess_' + String(game.incorrectGuesses);
+        console.log(newClass);
+        $apples.attr('class', newClass);
+      }
+
+      if (game.gameWon()) {
+        game.endGame("You win!", 'win');
+      }
+
+      if (game.guessesRemaining()) {
+        game.endGame("Sorry! You're out of guesses", 'lose');
+      }
     }
-  }
+  };
+
+  $replayLink.on('click', function(event) {
+    event.preventDefault();
+    game.reset();
+  });
+
 });
