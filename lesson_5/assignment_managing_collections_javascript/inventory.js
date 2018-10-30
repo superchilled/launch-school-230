326c338dc07fcfe1aa6bdba5de0f3ff4159087fd
superchilled <karl@superchilled.co.uk>
2018-10-30 18:27:49 +0000

    Adding finished solution code for managing collections assignment.

--- lesson_5/assignment_managing_collections_javascript/inventory.js
+++ lesson_5/assignment_managing_collections_javascript/inventory.js
@@ -3,22 +3,68 @@ var inventory;
 (function() {
   inventory = {
     collection: [],
-
+    currentID: 0,
     setDate: function() {
       var myDate = new Date();
       $('#order_date').text(myDate.toUTCString());
     },
     setTemplate: function() {
       var $inventoryItemTemplate = $('#inventory_item');
-      this.template = $inventoryItemTemplate.text();
+      this.template = $inventoryItemTemplate.text().trim();
       $inventoryItemTemplate.remove();
     },
     init: function() {
       this.setDate();
       this.setTemplate();
     },
-  };
+    addItem: function() {
+      var itemID = this.getID();
+      this.collection.push(this.createItem(itemID));
+      var itemMarkup = this.template.replace(/ID/g, String(itemID));
+      $('#inventory').append(itemMarkup);
+    },
+    getID: function() {
+      var id = this.currentID;
+      this.currentID++;
+      return id;
+    },
+    createItem: function(id) {
+      return {
+        id: id,
+        name: '',
+        stockNumber: '',
+        quantity: 1,
+      };
+    },
+    updateCollectionObject: function(itemID, fieldName, fieldValue) {
+      var object = this.findObjectInCollection(itemID);
+      var objectField = this.translateFieldName(fieldName);
+      object[objectField] = fieldValue;
+    },
+    findObjectInCollection: function(id) {
+      return this.collection.filter(function(obj) {
+        if (id === obj.id) {
+          return true;
+        }
+      })[0];
+    },
+    translateFieldName: function(name) {
+      var name = name.split('_')[1];
 
+      if (name === 'stock') {
+        return 'stockNumber';
+      } else {
+        return name;
+      }
+    },
+    removeObjectFromCollection: function(objectID) {
+      this.collection = this.collection.filter(function(object) {
+        if (objectID !== object.id) {
+          return true;
+        }
+      });
+    },
+  };
 })();
 
 $(function() {
@@ -26,6 +72,23 @@ $(function() {
 
   $('#add_item').on('click', function(event) {
     event.preventDefault();
-    inventory.addItem(item);
+    inventory.addItem();
+  });
+
+  $('#inventory').on('blur', 'input', function(event) {
+    var $field = $(event.target);
+    var fieldName = $field.attr('name');
+    var fieldValue = $field.val();
+    var itemID = Number(fieldName.slice(-1));
+    inventory.updateCollectionObject(itemID, fieldName, fieldValue);
+  });
+
+  $('#inventory').on('click', 'a[class=delete]', function(event) {
+    event.preventDefault();
+
+    var $tableRow = $(this).closest('tr');
+    var objectID = $tableRow.find('input[name^=item_id]').val();
+    inventory.removeObjectFromCollection(Number(objectID));
+    $tableRow.remove();
   });
 });
